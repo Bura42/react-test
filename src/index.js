934d7fdce0bfef77cc3c88f84f867c2acfdc3721
m.horiachev <m.horiachev@levi9.con>
2019-04-15 16:23:22 +0300

    rebuild with components rebuild with classes

--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom';
-import AppHeader from './components/app-header';
-import SearchPanel from './components/search-panel';
-import TodoList from './components/todo-list';
-
-const App = () => {
+import ReactDOM from 'react-dom';
 
 
-    const todoData = [
-        {label:'drink coffee', important:false, id: 1},
-        {label:'Make aws app', important:true, id: 2},
-        {label:'have a lunch', important:false, id: 3}
-    ];
-
-    const isLoggedIn = false;
-    const loginBox = <span>Log in please</span>;
-    const WelcomeBox = <span>Welcome Back</span>
-
-    return(
-        <div>
-            { isLoggedIn ? WelcomeBox : loginBox }
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList todos={todoData}/>
-        </div>
-    );
-};
+import App from './components/app'
 
 
-ReactDom.render(<App/>,document.getElementById('root'));
-
+ReactDOM.render(<App />,
+  document.getElementById('root'));
